564b999a94f94faaa9429afc75ca63b568a334a4
Emil Berberg Olsen <emilberolsen@gmail.com>
2020-03-16 14:25:27 +0100

    u

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="Categories">
         <div class="Category" v-for="(c, index) in Categories" :key="c+index">
-      <div class="catName">{{c.name}} <button @click="setVisible(c)">+</button></div>
+      <div class="catName">{{c.name}} <button class="eye" @click="setVisible(c)">
+        <span v-if="!c.isVisible"><i class="fas fa-eye"></i></span>
+        <span class="red" v-if="c.isVisible"><i class="fas fa-eye-slash"></i></span>
+        </button></div>
       <div class="Topics" v-if="c.isVisible">
-        <input class="Search" type="search" :placeholder="'Search in ' + c.name" v-model="c.search">
-        <div class="Topic" v-for="(t, index) in filterTopics(c)" :key="t+index">{{t.name}}</div>
-        <div class="Pagination"><button @click="prev(c)">Prev</button> <button @click="next(c)">Next</button></div>
+        <input class="Search" type="search" :placeholder="'Search in ' + c.name" @input="setSearch($event, c)">
+        <div class="box" v-if="hide(c) > 0">
+          <div class="Topic" v-for="(t, index) in filterTopics(c)" :key="t+index">{{t.name}}</div>
+        </div>
+        <div v-else class="noContent">No content</div>
+        <div class="Pagination">
+          <button class="btn" :class="{'btnDisabled': c.pagination[0] === 0}" @click="prev(c)">Prev</button>
+          <button class="btn" :class="{'btnDisabled': c.pagination[1] >= checkSize(c)}" @click="next(c)">Next</button>
+          </div>
       </div>
     </div>
   </div>
@@ -34,8 +43,33 @@ export default {
     filterTopics (c) {
       return this.Topics.filter(t => t.id === c.id && t.name.toLowerCase().includes(c.search.toLowerCase())).slice(c.pagination[0], c.pagination[1])
     },
-    prev (c) { c.pagination[0] -= 5; c.pagination[1] -= 5; this.$forceUpdate() },
-    next (c) { c.pagination[0] += 5; c.pagination[1] += 5; this.$forceUpdate() }
+    prev (c) {
+      if (c.pagination[0] >= 1) {
+        c.pagination[0] -= 5
+        c.pagination[1] -= 5
+        this.$forceUpdate()
+      }
+    },
+    next (c) {
+      let checkSize = this.Topics.filter(t => t.id === c.id).length
+      if (c.pagination[1] < checkSize) {
+        c.pagination[0] += 5
+        c.pagination[1] += 5
+        this.$forceUpdate()
+      }
+    },
+    setSearch (e, c) {
+      c.search = e.target.value
+      c.pagination[0] = 0
+      c.pagination[1] = 5
+    },
+    checkSize (c) {
+      return this.Topics.filter(t => t.id === c.id).length
+    },
+    hide (c) {
+      let check = this.Topics.filter(t => t.id === c.id).length
+      return check
+    }
   }
 }
 </script>
