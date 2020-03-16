364f14f51f2105e400c2d5edaeea3326c40bba4a
Emil Berberg Olsen <emilberolsen@gmail.com>
2020-03-16 15:31:12 +0100

    d

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="Categories">
+    <div class="addGroup">
+      <select class="group" @change="setTag($event)">
+        <option value="42">VUE</option>
+        <option value="43">ANGULAR</option>
+        <option value="44">REACT</option>
+        <option value="45">JQUERY</option>
+      </select>
+      <input class="group" type="text" placeholder="Title" @input="setTitle($event)">
+      <button class="group" @click="addMe" :class="{'disabled': addTopic.title.length <5}">ADD</button>
+    </div>
         <div class="Category" v-for="(c, index) in Categories" :key="c+index">
       <div class="catName">{{c.name}} <button class="eye" @click="setVisible(c)">
         <span v-if="!c.isVisible"><i class="fas fa-eye"></i></span>
@@ -8,7 +18,15 @@
       <div class="Topics" v-if="c.isVisible">
         <input class="Search" type="search" :placeholder="'Search in ' + c.name" @input="setSearch($event, c)">
         <div class="box" v-if="hide(c) > 0">
-          <div class="Topic" v-for="(t, index) in filterTopics(c)" :key="t+index">{{t.name}}</div>
+          <div class="Topic" v-for="(t, index) in filterTopics(c)" :key="t+index">
+            <span class="name">{{t.name}}</span>
+            <div class="btnGroup">
+              <input v-if="t.editMode" class="aInput" type="text" :placeholder="t.name" v-model="t.name">
+              <button @click="t.editMode = !t.editMode" class="aButton"><i class="fas fa-cog"></i></button>
+              <button @click="deleteMe(t)" class="aButton"><i class="fas fa-trash"></i></button>
+              <button class="aButton"><i class="fas fa-eye"></i></button>
+            </div>
+          </div>
         </div>
         <div v-else class="noContent">No content</div>
         <div class="Pagination">
@@ -25,7 +43,12 @@ import { mapGetters } from 'vuex'
 export default {
   name: 'Categories',
   data () {
-    return {}
+    return {
+      addTopic: {
+        tag: 42,
+        title: ''
+      }
+    }
   },
   computed: {
     ...mapGetters([
@@ -69,6 +92,16 @@ export default {
     hide (c) {
       let check = this.Topics.filter(t => t.id === c.id).length
       return check
+    },
+    deleteMe (topic) {
+      let index = this.Topics.indexOf(topic)
+      this.Topics.splice(index, 1)
+    },
+    setTag (e) { this.addTopic.tag = e.target.value },
+    setTitle (e) { this.addTopic.title = e.target.value },
+    addMe () {
+      let topic = { id: parseInt(this.addTopic.tag), name: this.addTopic.title, editMode: false }
+      if (this.addTopic.tag !== null && this.addTopic.title.length >= 5) this.Topics.push(topic)
     }
   }
 }
